8895c5e378b14e668519237e6df5126b126e142a
GeoffMcLennan <geoff.d.mclennan@gmail.com>
2022-05-15 20:44:21 -0700

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 import './App.css';
 import { ActiveSong } from './features/activeSong/activeSong';
+import { Homepage } from './features/homepage/homepage';
 
 function App() {
   return (
     <div className="App">
-        <ActiveSong />
+        <Router>
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path='/play' element={<ActiveSong />} />
+          </Routes>
+        </Router>
     </div>
   );
 }
---
+++ src/features/homepage/homepage.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export function Homepage() {
+  return (
+    <div>
+      <h1>Stream Share</h1>
+      <Link to='play'>Play</Link>
+    </div>
+  )
+}
