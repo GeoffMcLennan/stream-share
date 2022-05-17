88a392ace8e18eca538e4d5fb01861963147c212
GeoffMcLennan <geoff.d.mclennan@gmail.com>
2022-05-16 19:35:37 -0700

    Added ingress for the album

--- src/features/homepage/homepage.tsx
+++ src/features/homepage/homepage.tsx
@@ -4,7 +4,8 @@ export function Homepage() {
   return (
     <div>
       <h1>Stream Share</h1>
-      <Link to='play'>Play</Link>
+      <p><Link to='play'>Play</Link></p>
+      <p><Link to='/a/ABM_3V6vKQqt6SGXufxUiY2mTL'>Album 1</Link></p>
     </div>
   )
 }
